d1b979ed07befba1230a7b329a4109ec27a34d26
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2023-02-23 01:51:51 +0300

    add navigqatio styles

--- src/components/AsideNavMenu/AsideNavMenu.tsx
+++ src/components/AsideNavMenu/AsideNavMenu.tsx
@@ -1,6 +1,40 @@
 import { FC } from 'react'
+import cn from 'classnames'
+import { mockNavItems } from '../../shared/mocks/navItems'
 import s from './asideNavMenu.module.scss'
 
 export const AsideNavMenu: FC = () => {
-  return <aside className={s.wrapper}></aside>
+  return (
+    <aside className={s.wrapper}>
+      <nav>
+        <a href='/' className={s.logoLink}>
+          <img
+            src='./assets/images/logo.svg'
+            alt='Skilla IS'
+            width={109}
+            height={28}
+          />
+        </a>
+        <ul>
+          {mockNavItems.map((item, index) => (
+            <li
+              key={index}
+              className={cn(s.navItem, { [s.active]: item.title === 'Звонки' })}
+            >
+              <a href='/' className={s.navItemLink}>
+                <img
+                  className={s.navItemImg}
+                  src={`./assets/images/icons/navigation/${item.image}`}
+                  alt=''
+                  width={24}
+                  height={24}
+                />
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  )
 }
---
+++ src/shared/mocks/navItems.ts
@@ -0,0 +1,12 @@
+export const mockNavItems = [
+  { title: 'Итоги', image: 'results.svg' },
+  { title: 'Заказы', image: 'orders.svg' },
+  { title: 'Сообщения', image: 'messages.svg' },
+  { title: 'Звонки', image: 'calls.svg' },
+  { title: 'Контрагенты', image: 'counterparties.svg' },
+  { title: 'Документы', image: 'documents.svg' },
+  { title: 'Исполнители', image: 'performers.svg' },
+  { title: 'Отчеты', image: 'reports.svg' },
+  { title: 'База знаний', image: 'database.svg' },
+  { title: 'Настройки', image: 'settings.svg' },
+]
