412a01cd63ed98b3ec0f8c578aa22ee8e6a4c821
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2023-02-25 00:52:13 +0300

    add exit button styles

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -31,6 +31,14 @@ export const UserMenu: FC<UserMenuProps> = ({ className }) => {
 
       {isUserMenuDropped && (
         <div className={s.droppedMenu}>
+          <button className={s.exitButton}>
+            <svg className={s.exitButtonImage}>
+              <path
+                d='M14 5L12.59 6.41L14.17 8H6V10H14.17L12.59 11.58L14 13L18 9L14 5ZM2 2H9V0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H9V16H2V2Z'
+                fill='#ADBFDF'
+              />
+            </svg>
+          </button>
           <div className={s.userTitle}>
             <p className={s.userName}>Упоров Кирилл</p>
             <p className={s.userDesc}>
