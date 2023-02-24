64b13e0d16b606a3432711e5333229dd80d31bde
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2023-02-24 15:44:35 +0300

    add header styles

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,6 +1,49 @@
 import { FC } from 'react'
+import cn from 'classnames'
 import s from './mainHeader.module.scss'
 
 export const MainHeader: FC = () => {
-  return <header className={s.header}></header>
+  return (
+    <header className={s.header}>
+      <div>Среда, 13 окт</div>
+      <div className={s.analytics}>
+        <div className={cn(s.analitisItem, s.green)}>
+          <p>
+            Новые звонки <span>20 из 30 шт</span>
+          </p>
+          <div className={s.progressBarWrapp}>
+            <div style={{ width: '69px' }}></div>
+          </div>
+        </div>
+        <div className={cn(s.analitisItem, s.yellow)}>
+          <p>
+            Качество разговоров <span>40%</span>
+          </p>
+          <div className={s.progressBarWrapp}>
+            <div style={{ width: '67px' }}></div>
+          </div>
+        </div>
+        <div className={cn(s.analitisItem, s.red)}>
+          <p>
+            Конверсия в заказ <span>67%</span>
+          </p>
+          <div className={s.progressBarWrapp}>
+            <div style={{ width: '78px' }}></div>
+          </div>
+        </div>
+      </div>
+
+      <input type={'search'} />
+
+      <select>
+        <option>ООО Грузчиков Сервис Запад</option>
+        <option selected>ИП Сидорова Александра Михайловна</option>
+        <option>ИП Иванов М.М.</option>
+      </select>
+
+      <div className={s.user}>
+        <img src='./assets/images/header-avatar.png' alt='Аватар' />
+      </div>
+    </header>
+  )
 }
