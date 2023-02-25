4986632dc8828ceeff6085eeb68e9e8dc757059a
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2023-02-26 01:57:29 +0300

    add RTQ Query

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import './styles/global.scss'
 
 const container = document.getElementById('root')!
@@ -8,6 +10,8 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
---
+++ src/services/api.ts
@@ -0,0 +1,71 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+interface Call {
+  id: number
+  partnership_id: string
+  partner_data: {
+    id: string
+    name: string
+    phone: string
+  }
+  date: string
+  date_notime: string
+  time: number
+  from_number: string
+  from_extension: string
+  to_number: string
+  to_extension: string
+  is_skilla: number
+  status: string
+  record: string
+  line_number: string
+  in_out: number
+  from_site: number
+  source: string
+  errors: string[]
+  disconnect_reason: string
+  results: [
+    {
+      type: string
+      title: string
+      tooltip: string
+    }
+  ]
+  stages: []
+  abuse: []
+  contact_name: string
+  contact_company: string
+  person_id: number
+  person_name: string
+  person_surname: string
+  person_avatar: string
+}
+
+type CallsResponse = {
+  total_rows: string
+  results: Call[]
+}
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://api.skilla.ru/mango/getList',
+  prepareHeaders: headers => {
+    headers.set('Authorization', `Bearer testtoken`)
+    return headers
+  },
+})
+
+export const api = createApi({
+  reducerPath: 'callsApi',
+  baseQuery: baseQuery,
+  endpoints: build => ({
+    getCalls: build.query<CallsResponse, string>({
+      query() {
+        return {
+          url: ``,
+          method: 'POST',
+          body: {},
+        }
+      },
+    }),
+  }),
+})
